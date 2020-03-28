75372baf3aa9a906d21101b4e3119923dfed587f
James Wagstaff <james.c.wagstaff@gmail.com>
2020-03-28 15:16:57 -0600

    Update config file

--- src/main/java/com/example/webflux/config/R2DBCConfig.java
+++ src/main/java/com/example/webflux/config/R2DBCConfig.java
@@ -3,22 +3,35 @@ package com.example.webflux.config;
 import io.r2dbc.postgresql.PostgresqlConnectionConfiguration;
 import io.r2dbc.postgresql.PostgresqlConnectionFactory;
 import io.r2dbc.spi.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
 
 @Configuration
 class R2DBCConfig extends AbstractR2dbcConfiguration {
+
+    @Value("${db.host}")
+    String host;
+    @Value("${db.port}")
+    Integer port;
+    @Value("${db.user}")
+    String user;
+    @Value("${db.pass}")
+    String pass;
+    @Value("${db.table}")
+    String database;
+
     @Bean
     @Override
     public ConnectionFactory connectionFactory() {
         return new PostgresqlConnectionFactory(
                 PostgresqlConnectionConfiguration.builder()
-                        .host("localhost")
-                        .port(5432)
-                        .username("postgres")
-                        .password("postgres")
-                        .database("postgres")
+                        .host(host)
+                        .port(port)
+                        .username(user)
+                        .password(pass)
+                        .database(database)
                         .build());
     }
 }
